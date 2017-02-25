907cb6b221f335c37432d8b27d6b46ef55ec14b1
Erik <erikpvc@gmail.com>
2017-02-25 03:59:54 +0100

    New update form?

--- @Resources/Updater/UpdaterPhase1.bat
+++ @Resources/Updater/UpdaterPhase1.bat
@@ -1,8 +1,10 @@
 cscript "Updater.vbs"
+echo Update downloaded!
 pause
 powershell.exe -Command "Set-ExecutionPolicy RemoteSigned -Scope CurrentUser"
+echo Policy Signed!
 pause
 powershell.exe -File "Updater.ps1"
+echo Files Extracted!
 pause
-del "..\..\..\Ilustro-Full-master.zip"
-pause
+call ..\..\..\Ilustro-Full-master\@Resources\Updater\UpdaterPhase2.bat
---
+++ @Resources/Updater/UpdaterPhase2.bat
@@ -0,0 +1,7 @@
+rd /q /s "..\..\..\Ilustro-Full"
+move ..\..\..\Ilustro-Full-master ..\..\..\Ilustro-Full
+echo Folder renamed!
+pause
+del "..\..\..\Ilustro-Full-master.zip"
+echo Zip deleted!
+pause
