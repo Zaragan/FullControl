4d8c9f8224529960af3123a1792d51ccd7b79895
Erik <erikpvc@gmail.com>
2017-02-25 02:42:08 +0100

    More tests...

--- @Resources/Updater/Updater.bat
+++ @Resources/Updater/Updater.bat
@@ -1,6 +1,10 @@
 cscript "Updater.vbs"
+pause
 rd /q /s "..\..\..\Ilustro-Full-master"
+pause
 powershell.exe -Command "Set-ExecutionPolicy RemoteSigned -Scope CurrentUser"
+pause
 powershell.exe -File "Updater.ps1"
+pause
 del "..\..\..\Ilustro-Full-master.zip"
 pause
