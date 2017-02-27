568455df846342e3bffd1b77369a06826bb45807
Erik <erikpvc@gmail.com>
2017-02-27 04:40:25 +0100

    Forgot the exit
    and deleted the echoes

--- @Resources/Updater/UpdaterPhase1.bat
+++ @Resources/Updater/UpdaterPhase1.bat
@@ -1,8 +1,6 @@
 @echo off
 cscript "Updater.vbs"
-echo Update downloaded!
 powershell.exe -Command "Set-ExecutionPolicy RemoteSigned -Scope CurrentUser"
-echo Policy Signed!
 powershell.exe -File "Updater.ps1"
-echo Files Extracted!
 start ..\..\..\Ilustro-Full-master\@Resources\Updater\UpdaterPhase2.bat
+exit
