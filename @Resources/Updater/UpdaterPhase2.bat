08185d666491274963fd39629c2d469d203e365c
Erik <erikpvc@gmail.com>
2017-02-27 04:39:32 +0100

    Final test

--- @Resources/Updater/UpdaterPhase2.bat
+++ @Resources/Updater/UpdaterPhase2.bat
@@ -1,8 +1,5 @@
-sleep 10
+@echo off
 rd /q /s "..\..\..\Ilustro-Full"
 move ..\..\..\Ilustro-Full-master ..\..\..\Ilustro-Full
-echo Folder renamed!
-pause
 del "..\..\..\Ilustro-Full-master.zip"
-echo Zip deleted!
-pause
+exit
