637ca780f59a12d138b976675e4e8ebd5e0c652a
Erik <erikpvc@gmail.com>
2017-02-27 05:40:49 +0100

    Delete UpdaterPhase1.bat

--- @Resources/Updater/UpdaterPhase1.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cscript "Updater.vbs"
-powershell.exe -Command "Set-ExecutionPolicy RemoteSigned -Scope CurrentUser"
-powershell.exe -File "Updater.ps1"
-start ..\..\..\Ilustro-Full-master\@Resources\Updater\UpdaterPhase2.bat
-exit
