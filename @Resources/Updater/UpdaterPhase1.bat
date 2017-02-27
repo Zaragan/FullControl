f119b40f39cbd1275a0fb5dbf0b09b98d5f0bfcc
Erik <erikpvc@gmail.com>
2017-02-27 04:38:34 +0100

    Things are working now...
    I think xD

--- @Resources/Updater/UpdaterPhase1.bat
+++ @Resources/Updater/UpdaterPhase1.bat
@@ -1,10 +1,8 @@
+@echo off
 cscript "Updater.vbs"
 echo Update downloaded!
-pause
 powershell.exe -Command "Set-ExecutionPolicy RemoteSigned -Scope CurrentUser"
 echo Policy Signed!
-pause
 powershell.exe -File "Updater.ps1"
 echo Files Extracted!
-pause
-call ..\..\..\Ilustro-Full-master\@Resources\Updater\UpdaterPhase2.bat
+start ..\..\..\Ilustro-Full-master\@Resources\Updater\UpdaterPhase2.bat
